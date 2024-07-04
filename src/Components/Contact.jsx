5f3ddf2c13fd676a0ea2c5d3752d8c08410cb37a
Yash Rana <itisyash24@gmail.com>
2024-07-04 23:44:07 +0530

    second

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { MdEmail } from "react-icons/md";
 
 function Contact() {
   return (
     <div className="text-center">
         <h3 className="text-md mt-10 py-1 dark:text-white">Get in Touch</h3>
-      <p></p>
+        <a href="mailto:yashkrana2002@gmail.com"><p className="text-white text-center flex justify-center"><MdEmail/></p></a>
     </div>
   )
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import code from "../assets/design.png"
+import code from "../assets/consulting.png"
+import freescribe from "../assets/design.png"
+import PFP from "../assets/PFP.png"
 function Projects() {
   return (
     <section className="">
@@ -28,7 +30,7 @@ function Projects() {
               <p className="text-gray-800 py-1">Appwrite</p>
           </div>
           <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-300 flex-1 flex flex-col items-center box-shadow-hover:hover transition duration-500 transform hover:scale-105 ">
-          <img className="items-center"src={code} width={100} height={100} />
+          <img className="items-center"src={PFP} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Old Portfolio
               </h3>
@@ -36,7 +38,9 @@ function Projects() {
                 Portfolio to direct clients to my Fiverr Page 
               </p>
               <span>
-              <a href="https://github.com/NotTheDriver/portfolio" target="_blank"><h4 className=" text-teal-600">Github</h4></a>
+              <span className="flex justify-center gap-3">
+              <a href="https://yash-animated-portfolio.netlify.app/" target="_blank" ><h4 className="py-4 text-teal-600">Website</h4></a>
+              <a href="https://github.com/NotTheDriver/portfolio" target="_blank"><h4 className="py-4 text-teal-600">GitHub</h4></a></span>
               {/* <h4 className="py-4 text-teal-600">Tools Used</h4> */}
               </span>
               
@@ -45,14 +49,16 @@ function Projects() {
               <p className="text-gray-800 py-1">Framer Motion</p>
           </div>
           <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-300 flex-1 flex flex-col items-center box-shadow-hover:hover transition duration-500 transform hover:scale-105 ">
-          <img className=""src={code} width={100} height={100} />
+          <img className=""src={freescribe} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Vaccination Management System
+                FreeScribe
               </h3>
               <p className="py-2">
-              Manages the vaccine inventory of a hospital for new and existing patients.
+              React web based transcription & translation app that uses web workers to run ML models in the browser
               </p>
-              <a href=""><h4 className="py-4 text-teal-600">GitHub</h4></a>
+              <span className="flex justify-center gap-3">
+              <a href="https://freescribe-me.netlify.app/" target="_blank"><h4 className="py-4 text-teal-600">Website</h4></a>
+              <a href="https://github.com/NotTheDriver/FreeScribe" target="_blank"><h4 className="py-4 text-teal-600">GitHub</h4></a></span>
               <p className="text-gray-800 py-1">C++</p>
           </div>
         </div>
